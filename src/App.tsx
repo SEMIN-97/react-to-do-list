ddda6679a5da76a3c6f9b61db74e442179b6fa07
SEMIN-97 <jsemin1021@gmail.com>
2024-02-03 15:44:42 +0900

    Style: font-face 위치 이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 
-import GlobalFonts from './fonts/fonts';
 import GlobalStyle from './styles/GlobalStyle';
 import ToDoLayout from './components/ToDoLayout';
 
 function App() {
   return (
     <>
-      <GlobalFonts />
       <GlobalStyle />
       <ToDoLayout />
     </>
