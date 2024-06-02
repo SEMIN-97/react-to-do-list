e123669c67fbd38873337030498b6be8a06b1504
SEMIN-97 <jsemin.1021@gmail.com>
2024-06-02 23:18:25 +0900

    Fix: MSW 분기 처리 및 localhost 오류 수정

--- src/index.tsx
+++ src/index.tsx
@@ -9,8 +9,25 @@ const root = document.getElementById('root') as HTMLElement;
 
 async function enableMocking() {
   const { worker } = await import('./mocks/browser');
+  const isLocalhost = Boolean(
+    window.location.hostname === 'localhost' ||
+    // [::1] is the IPv6 localhost address.
+    window.location.hostname === '[::1]' ||
+    // 127.0.0.0/8 are considered localhost for IPv4.
+    window.location.hostname.match(
+      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+    )
+  );
 
-  return worker.start();
+  const serviceWorkerUrl = isLocalhost
+    ? './mockServiceWorker.js'
+    : '/react-to-do-list/mockServiceWorker.js';
+
+  return worker.start({
+    serviceWorker: {
+      url: serviceWorkerUrl
+    }
+  });
 }
 
 enableMocking().then(() => {
